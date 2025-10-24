aa5ccb75723e68e6ba29c63bce9c663d21cb78f9
hugo-chilemme <hugo.chilemme@gmail.com>
2025-10-25 01:03:45 +0200

    Close mobile menu on navigation item click

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -75,6 +75,7 @@ export default function Header() {
 										<Link
 											key={item.label}
 											href={item.href}
+											onClick={() => setMobileMenuOpen(false)}
 											className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
 										>
 											{item.label}
